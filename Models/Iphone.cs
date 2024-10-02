6cea8253d5ed7abc575d2a6e04a0bdbd9389843d
Joao <joao_neto100@hotmail.com>
2024-10-02 10:49:35 -0300

    Desafio-Smartphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+     // Construtor que chama o construtor base da classe Smartphone
+        public Iphone(string numero, string modelo, string imei, int memoria) 
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // Sobrescrevendo o método abstrato "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no iPhone.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Testando a classe Nokia
+        Nokia nokia = new Nokia("30207870", "Nokia 1100", "2845678456", 128);
+        Console.WriteLine("Testando Nokia:");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+        Console.WriteLine();
+
+        // Testando a classe Iphone
+        Iphone iphone = new Iphone("987654321", "iPhone 14", "2765612556", 256);
+        Console.WriteLine("Testando iPhone:");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
